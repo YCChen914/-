f463a479678d0546b6e65eb6e07c7d5f1d3b4ec8
PaulChen <81601297+YCChen914@users.noreply.github.com>
2022-09-08 12:38:01 +0800

    復原修正項

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
-#import app.model as model
+import app.model as model
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
-"""
 @app.route('/predict', methods=['POST'])
 def predict():
     insertValues = request.get_json()
@@ -19,4 +15,3 @@ def predict():
     print(input)
     result = model.result()
     return jsonify({'return':str(result)})
-"""   
