24750eab9653c1beb5e9a14ca6c2ac761bfe4c60
PaulChen <csz890914@gmail.com>
2022-09-13 20:05:04 +0800

    網頁美化

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 #-*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!'
-
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
